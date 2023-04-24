1c098ea68951f024f45dba0112b96f6ee87304d6
Bilal Ahmad <bilalahmad4554@gmail.com>
2023-04-24 07:10:15 +0500

    First commit.

---
+++ example/lib/util.dart
@@ -0,0 +1,30 @@
+import 'package:health/health.dart';
+
+/// List of data types available on iOS
+const List<HealthDataType> dataTypesIOS = [
+  HealthDataType.RESPIRATORY_RATE
+];
+
+/// List of data types available on Android
+const List<HealthDataType> dataTypesAndroid = [
+  // HealthDataType.ACTIVE_ENERGY_BURNED,
+  // HealthDataType.BLOOD_GLUCOSE,
+  // HealthDataType.BLOOD_OXYGEN,
+  // HealthDataType.BLOOD_PRESSURE_DIASTOLIC,
+  // HealthDataType.BLOOD_PRESSURE_SYSTOLIC,
+  // HealthDataType.BODY_FAT_PERCENTAGE,
+  // HealthDataType.HEIGHT,
+  // HealthDataType.WEIGHT,
+  // HealthDataType.RESPIRATORY_RATE,
+  // // HealthDataType.BODY_MASS_INDEX,
+  // HealthDataType.BODY_TEMPERATURE,
+  // HealthDataType.HEART_RATE,
+  // HealthDataType.STEPS,
+  // // HealthDataType.MOVE_MINUTES, // TODO: Find alternative for Health Connect
+  // HealthDataType.DISTANCE_DELTA,
+  // // HealthDataType.SLEEP_AWAKE,
+  // // HealthDataType.SLEEP_ASLEEP,
+  // // HealthDataType.SLEEP_IN_BED,
+  // HealthDataType.WATER,
+  // HealthDataType.WORKOUT,
+];
